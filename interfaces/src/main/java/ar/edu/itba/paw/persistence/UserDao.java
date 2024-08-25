97719e74138d7c022c16107d941646bba1e61c26
matiasdaneri <mdaneri@itba.edu.ar>
2024-08-24 23:00:11 -0300

    Basic expected functionality for UserDao

--- interfaces/src/main/java/ar/edu/itba/paw/persistence/UserDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/persistence/UserDao.java
@@ -2,10 +2,17 @@ package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserDao {
 
     Optional<User> findById(long id);
+    List<User> findAll();
 
+    int save(User user);
+
+    int update(User user);
+
+    int deleteById(long id);
 }
