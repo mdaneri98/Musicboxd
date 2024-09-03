d5aa867d115faa749cf8cae9124c353901bd9b08
matiasdaneri <mdaneri@itba.edu.ar>
2024-09-03 11:38:14 -0300

    Advanced over asking verification after a review

--- services/src/main/java/ar/edu/itba/paw/services/email/EmailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/email/EmailServiceImpl.java
@@ -1,20 +1,24 @@
 package ar.edu.itba.paw.services.email;
 
+import ar.edu.itba.paw.User;
+import ar.edu.itba.paw.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
 import java.util.Map;
 
-@Service("emailService")
-public class EmailServiceImpl  {
+@Service
+public class EmailServiceImpl implements EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
@@ -41,6 +45,19 @@ public class EmailServiceImpl  {
         thymeleafContext.setVariables(params);
         String htmlBody = thymeleafTemplateEngine.process(template, thymeleafContext);
         sendHtmlMessage(to, subject, htmlBody);
+    }
+
+    @Async
+    public void sendVerificationForUser(User user) throws MessagingException {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("userId", user.getId());
+        params.put("userEmail", user.getEmail());
+        this.sendMessageUsingThymeleafTemplate(
+                "user_verification",
+                user.getEmail(),
+                "MusicBoxd - Verification",
+                params
+        );
     }
 
 
