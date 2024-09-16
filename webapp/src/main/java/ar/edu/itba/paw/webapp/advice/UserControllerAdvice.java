fc0ce328012e0ab6c666d97228d194f36b3962c4
hmarc953 <h.marc953@gmail.com>
2024-09-16 05:11:03 -0300

    Fixed Controller Advice and added Loggin

--- webapp/src/main/java/ar/edu/itba/paw/webapp/advice/UserControllerAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/advice/UserControllerAdvice.java
@@ -2,6 +2,8 @@ package ar.edu.itba.paw.webapp.advice;
 
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.auth.AuthCUserDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +12,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class UserControllerAdvice {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserControllerAdvice.class);
+
     @ModelAttribute(value = "loggedUser", binding = false)
     public User getLoggedUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth.getPrincipal() instanceof AuthCUserDetails pud) {
-            return pud.getUser();
+        if(auth != null) {
+            LOGGER.info("Authentication object: {}", auth);
+            if (auth.getPrincipal() instanceof AuthCUserDetails pud) {
+                LOGGER.info("Logged-in user: {}", pud.getUser());
+                return pud.getUser();
+            } else {
+                LOGGER.warn("Principal is not an instance of AuthCUserDetails");
+            }
+        } else {
+            LOGGER.warn("Authentication object is null");
         }
         return null;
     }
