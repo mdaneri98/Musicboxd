26e8351766c302beedb8e2c12e48737f2865ae41
matidaneri <mdaneri@matis-MacBook-Pro.local>
2024-10-29 17:13:07 -0300

    Verifications and password change working

--- models/src/main/java/ar/edu/itba/paw/models/UserVerification.java
+++ models/src/main/java/ar/edu/itba/paw/models/UserVerification.java
@@ -22,7 +22,7 @@ public class UserVerification {
     private String verificationType;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public UserVerification() {
