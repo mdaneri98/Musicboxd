cc67b9951891386ecdd6c6a6e94aea9c8df967b1
Matias Daneri <mdaneri98@gmail.com>
2025-12-13 11:46:10 -0300

    Back to previous cors configuration

--- api/src/main/java/ar/edu/itba/paw/api/config/ApiAuthConfig.java
+++ api/src/main/java/ar/edu/itba/paw/api/config/ApiAuthConfig.java
@@ -43,10 +43,12 @@ public class ApiAuthConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
